af185554e88c58d6ff8cf3cf5a04bf5b8cc0e716
leyliperea <160614703+leyliperea@users.noreply.github.com>
2024-02-28 22:51:53 -0600

    Update soko.py

--- soko.py
+++ soko.py
@@ -1,3 +1,13 @@
+"""
+1-Cajas
+2-Metas
+3-Paredes
+4-Piso
+5-Personaje meta
+6-caja meta
+
+
+#define el mapa de juego
 mapa = [3,4,4,0,4,4,3]
 #definimos la posicion inicial del personaje
 posicion colulmna = 3  constant name "posicion_columna" doesn't conform to UPPER_CASE name
@@ -7,6 +17,19 @@ print(mapa)
 movimiento = input("selecciona el movimiento:")
 
 if movimiento == 'd':
-    mapa[posicion_columna] = 4#   trailing whitespace
-    mapa[posicion_columna + 1] =0
-    posicion_columna+=1
+    #movimiento 1: [0,4] -> [4,0]
+    if mapa [posicion_columna] == 0 and mapa [posicion_columna+1] ==4:
+        #donde estaba el persona pone un piso
+        mapa[posicion_columna] = 4 
+        #donde estaba el piso pone el personaje
+        mapa[posicion_columna + 1] = 0
+        #actualiza la posicion del personaje
+        posicion_columna+=1
+# moverse a la izquierda 
+    if movimiento =='a':
+       #movimiento 2: [4,0] -> [0,4]
+        if mapa[posicion_columna-1] ==4 and mapa [posicion_columna] ==0:
+            mapa[posicion_columna-1] = 0
+            mapa[posicion_columna] = 4
+            posicion_columna-=1
+        
